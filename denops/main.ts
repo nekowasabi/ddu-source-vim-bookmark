a078de873d29b5ef23a06e766a14b711597bd76f
nekowasabi <nolifeking00@gmail.com>
2024-02-05 20:19:28 +0900

    feat: ブックマークのソースを追加

--- denops/main.ts
+++
@@ -1,7 +0,0 @@
-import {
-  BaseSource,
-  DduOptions,
-  Item,
-  SourceOptions,
-} from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
-import { Denops, fn, op, vars } from "https://deno.land/x/ddu_vim@v3.10.2/deps.ts";
